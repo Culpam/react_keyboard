74a4dd25b9e9c7529dd4d79544a04910bb3e80e5
Dmytro Hrabchak <dmytro@MacBook-Air-Dmytro.local>
2025-08-09 19:49:01 +0300

    uncomment Class Component and comment FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+// import { useEffect, useState } from 'react';
 
 // #region ClassComponent
-// type State = {
-//   pressedKey: string | null;
-// };
+type State = {
+  pressedKey: string | null;
+};
 
-// export class App extends React.Component<State> {
-//   state: Readonly<State> = {
-//     pressedKey: null,
-//   };
+export class App extends React.Component<State> {
+  state: Readonly<State> = {
+    pressedKey: null,
+  };
 
-//   componentDidMount(): void {
-//     document.addEventListener('keyup', this.handleKeyup);
-//   }
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyup);
+  }
 
-//   componentWillUnmount(): void {
-//     document.removeEventListener('keyup', this.handleKeyup);
-//   }
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyup);
+  }
 
-//   handleKeyup = (event: KeyboardEvent) => {
-//     this.setState({ pressedKey: event.key });
-//   };
+  handleKeyup = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
 
-//   render() {
-//     const { pressedKey } = this.state;
+  render() {
+    const { pressedKey } = this.state;
 
-//     return (
-//       <div className="App">
-//         {pressedKey ? (
-//           <p className="App__message">The last pressed key is [{pressedKey}]</p>
-//         ) : (
-//           <p className="App__message">Nothing was pressed yet</p>
-//         )}
-//       </div>
-//     );
-//   }
-// }
+    return (
+      <div className="App">
+        {pressedKey ? (
+          <p className="App__message">The last pressed key is [{pressedKey}]</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
+      </div>
+    );
+  }
+}
 // #endregion
 
 // #region FunctionalComponent
-export const App: React.FC = () => {
-  const [pressedKey, setPressedKey] = useState<string | null>(null);
+// export const App: React.FC = () => {
+//   const [pressedKey, setPressedKey] = useState<string | null>(null);
 
-  useEffect(() => {
-    const handleKeyup = (event: KeyboardEvent) => {
-      setPressedKey(event.key);
-    };
+//   useEffect(() => {
+//     const handleKeyup = (event: KeyboardEvent) => {
+//       setPressedKey(event.key);
+//     };
 
-    document.addEventListener('keyup', handleKeyup);
+//     document.addEventListener('keyup', handleKeyup);
 
-    return () => {
-      document.removeEventListener('keyup', handleKeyup);
-    };
-  }, []);
+//     return () => {
+//       document.removeEventListener('keyup', handleKeyup);
+//     };
+//   }, []);
 
-  return (
-    <div className="App">
-      {pressedKey ? (
-        <p className="App__message">The last pressed key is [{pressedKey}]</p>
-      ) : (
-        <p className="App__message">Nothing was pressed yet</p>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div className="App">
+//       {pressedKey ? (
+//         <p className="App__message">The last pressed key is [{pressedKey}]</p>
+//       ) : (
+//         <p className="App__message">Nothing was pressed yet</p>
+//       )}
+//     </div>
+//   );
+// };
 // #endregion
